a450d3efca15f47b0cff8ce2075cfc3d9ef9ee27
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2022-12-13 14:51:45 +0200

    Rocks movement fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let stage, w, h, loader, canvas;
-let background, grant, ground, hill, hill2;
-let shouldUpdate = true
+let canvas, stage, w, h;
+let loader;
+let update = true;
 
 function init() {
     stage = new createjs.Stage("gameCanvas");
@@ -17,42 +17,65 @@ function init() {
     createjs.Touch.enable(stage);
     stage.enableMouseOver(10);
     loader = new createjs.LoadQueue(true);
-    loader.addEventListener("complete", handleComplete);
+    loader.addEventListener("complete", handleImageLoad);
     loader.loadManifest(manifest, true, "./assets/");
 }
 
-function handleComplete() {
-    background = new createjs.Shape();
-    background.graphics.beginBitmapFill(loader.getResult("background")).drawRect(0, 0, w, h);
+function stop() {
+    createjs.Ticker.removeEventListener("tick", tick);
+}
+
+function handleImageLoad(event) {
     let container = new createjs.Container();
-    let rock;
+    let background = new createjs.Shape();
 
-    for (let i = 0; i < 100; i++) {
-        rock = new createjs.Bitmap(loader.getResult("rock"));
-        container.addChild(rock);
-        rock.x = canvas.width * Math.random() | 0;
-        rock.y = canvas.height * Math.random() | 0;
-        rock.regX = rock.image.width / 2 | 0;
-        rock.regY = rock.image.height / 2 | 0;
-        rock.cursor = 'pointer'
-        rock.scale = rock.originalScale = Math.random() * 0.4 + 0.6;
-        rock.name = "bmp_" + i;
+    let bitmap;
+    background.graphics.beginBitmapFill(loader.getResult('background')).drawRect(0, 0, w, h)
+    background.x = 0
+    background.y = 0
+    stage.addChild(background)
+    stage.addChild(container);
 
-        rock.addEventListener('pressmove', function (evt) {
-            shouldUpdate = true
-        })
+    for (let i = 0; i < 20; i++) {
+        bitmap = new createjs.Bitmap(loader.getResult("rock"));
+        container.addChild(bitmap);
+        bitmap.x = canvas.width * Math.random() | 0;
+        bitmap.y = canvas.height * Math.random() | 0;
+        bitmap.regX = bitmap.image.width / 2 | 0;
+        bitmap.regY = bitmap.image.height / 2 | 0;
+        bitmap.cursor = 'pointer'
+        bitmap.scale = bitmap.originalScale = Math.random() * 0.2 + 0.4;
+        bitmap.name = "rock_" + i;
+
+        bitmap.on("mousedown", function (evt) {
+            this.parent.addChild(this);
+            this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
+        });
+
+        bitmap.on("pressmove", function (evt) {
+            this.x = evt.stageX + this.offset.x;
+            this.y = evt.stageY + this.offset.y;
+            update = true;
+        });
+
+        bitmap.on("rollover", function (evt) {
+            this.scale = this.originalScale * 1.2;
+            update = true;
+        });
+
+        bitmap.on("rollout", function (evt) {
+            this.scale = this.originalScale;
+            update = true;
+        });
+
     }
 
-    stage.addChild(background)
-
-    createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", tick);
-
 }
 
 function tick(event) {
-    if (shouldUpdate) {
-        shouldUpdate = false
+    if (update) {
+        update = false;
         stage.update(event);
         console.log('update')
     }
