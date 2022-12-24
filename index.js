3dd5cda8887486ae4d82a8f9a26a82c108633669
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2022-12-24 13:02:24 +0200

    Reworked for snowballs clicker

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let canvas, stage, w, h;
 let loader;
-let update = true;
+let snowballSpeedMultiplier = 1;
+let score = 0;
+let snowballsContainer;
+let baseSnowballSpawnTime = 2000;
+let gameOver = false;
+let lives = 3;
+let scoreText, livesText;
+let snowballSpeed = 3000
 
 function init() {
     stage = new createjs.Stage("gameCanvas");
@@ -10,8 +17,9 @@ function init() {
     canvas = stage.canvas
 
     let manifest = [
-        {src: "background.jpg", id: "background"},
-        {src: "rock.png", id: "rock"},
+        {src: "background.png", id: "background"},
+        {src: "snowball.png", id: "rocket"},
+        {src: "explotion.gif", id: "explotion"}
     ];
 
     createjs.Touch.enable(stage);
@@ -26,57 +34,99 @@ function stop() {
 }
 
 function handleImageLoad(event) {
-    let container = new createjs.Container();
+
+    snowballsContainer = new createjs.Container();
     let background = new createjs.Shape();
 
-    let bitmap;
     background.graphics.beginBitmapFill(loader.getResult('background')).drawRect(0, 0, w, h)
     background.x = 0
     background.y = 0
     stage.addChild(background)
-    stage.addChild(container);
+    stage.addChild(snowballsContainer);
 
-    for (let i = 0; i < 20; i++) {
-        bitmap = new createjs.Bitmap(loader.getResult("rock"));
-        container.addChild(bitmap);
-        bitmap.x = canvas.width * Math.random() | 0;
-        bitmap.y = canvas.height * Math.random() | 0;
-        bitmap.regX = bitmap.image.width / 2 | 0;
-        bitmap.regY = bitmap.image.height / 2 | 0;
-        bitmap.cursor = 'pointer'
-        bitmap.scale = bitmap.originalScale = Math.random() * 0.2 + 0.4;
-        bitmap.name = "rock_" + i;
 
-        bitmap.on("mousedown", function (evt) {
-            this.parent.addChild(this);
-            this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
-        });
+    createScore()
+    createLives()
+    handleSnowballSpawn()
 
-        bitmap.on("pressmove", function (evt) {
-            this.x = evt.stageX + this.offset.x;
-            this.y = evt.stageY + this.offset.y;
-            update = true;
-        });
+    createjs.Ticker.timingMode = createjs.Ticker.RAF
+    createjs.Ticker.addEventListener("tick", tick);
+    createjs.Ticker.addEventListener("tick", checkCollision);
+}
 
-        bitmap.on("rollover", function (evt) {
-            this.scale = this.originalScale * 1.2;
-            update = true;
-        });
+//Я хз чого я захотiв тут рекурсiю
+function handleSnowballSpawn() {
+    if (gameOver) return
+    rocket = new createjs.Bitmap(loader.getResult("rocket"))
+    let rocketX = canvas.width * Math.random() < 150 ? 150 : canvas.width * Math.random() | 150
+    snowballsContainer.addChild(rocket);
+        rocket.x = rocketX - 100
+        rocket.y = -300
+        rocket.regX = 50
+        rocket.regY = 50
+        rocket.cursor = 'pointer'
+        rocket.name = "snowball_" + score;
 
-        bitmap.on("rollout", function (evt) {
-            this.scale = this.originalScale;
-            update = true;
-        });
+    rocket.addEventListener('click', handleClickOnSnowball)
+    let timeForNextSnowball = baseSnowballSpawnTime - score > 500 ? 2000 - score : 500
+    createjs.Tween.get(rocket)
+        .to({ y: h, rotation: 360 }, snowballSpeed * snowballSpeedMultiplier)
+    setTimeout(() => {
+        handleSnowballSpawn()
+    }, timeForNextSnowball)
+}
+
+function handleClickOnSnowball(event) {
+    let snowball = event.target;
+    score += 10
+    snowballsContainer.removeChild(snowball)
+    scoreText.updateCache()
+}
+
+function checkCollision() {
+    snowballsContainer.children.forEach(snowball => {
+        if(snowball.y >= h - 250) {
+            snowballsContainer.removeChild(snowball)
+            lives--
+            livesText.updateCache()
+        }
+    })
+    if (lives <= 0) {
+        let gameOverText = new createjs.Text('Game over!', "bold 128px Arial", '#000000')
+        gameOverText.textAlign = 'center'
+        gameOverText.textBaseline = 'middle'
+        gameOverText.x = w / 2
+        gameOverText.y = h / 2
+        stage.addChild(gameOverText)
+        gameOver = true
+        stage.update()
+        stop()
 
     }
+}
 
-    createjs.Ticker.addEventListener("tick", tick);
+function createLives() {
+    livesText = new createjs.Text(`Lives: ${lives}`, 'bold 48px Arial', '#000000')
+    livesText.textAlign = 'center'
+    livesText.textBaseline = 'middle'
+    livesText.x = w - 200
+    livesText.y = 40
+    let bounds = livesText.getBounds()
+    livesText.cache(-140, -40, bounds.width * 3 + Math.abs(bounds.x), bounds.height + Math.abs(bounds.y))
+    stage.addChild(livesText)
+}
+
+function createScore() {
+    scoreText = new createjs.Text(`Score: ${score}`, 'bold 48px Arial', '#000000')
+    scoreText.textAlign = 'center'
+    scoreText.textBaseline = 'middle'
+    scoreText.x = 140
+    scoreText.y = 40
+    let bounds = scoreText.getBounds()
+    scoreText.cache(-140, -40, bounds.width * 3 + Math.abs(bounds.x), bounds.height + Math.abs(bounds.y))
+    stage.addChild(scoreText)
 }
 
 function tick(event) {
-    if (update) {
-        update = false;
-        stage.update(event);
-        console.log('update')
-    }
+    stage.update(event);
 }
